1d9917759286628fdff577eeef58e0f5a0ad89c9
annimorphism <trivediannirudha@gmail.com>
2021-07-13 14:47:58 +0530

    Changed to Accept different IMG Format

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const fetch = require("node-fetch");
 const execSync = require("child_process").execSync;
 const wallpaper = require("wallpaper");
+const puppeteer = require("puppeteer");
 
 const key = process.env.api_key;
 let url = "https://api.nasa.gov/planetary/apod";
@@ -12,15 +13,35 @@ async function fetcher() {
     let response = await fetch(hit_url);
     let data = await response.json();
     let img = data.hdurl ? data.hdurl : data.url;
-    execSync(`curl -o ./Img/background.jpg ${img}`);
-    setBackground();
+    let imgext = data.hdurl ? ".jpg" : ".png";
+    patt = `./Img/background${imgext}`;
+    console.log(patt);
+    let x = img.includes("gltf");
+    if (x) {
+      async function screen() {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(img);
+        await page.setViewport({
+          width: 1920,
+          height: 1080,
+        });
+        await page.screenshot({ path: patt });
+        await browser.close();
+      }
+      screen();
+    } else {
+      execSync(`curl -o ${patt} ${img}`);
+    }
+    setBackground(patt);
   } catch (e) {
     console.log("There was some error -> ", e);
   }
 }
-async function setBackground() {
-  let path = "./Img/background.jpg";
-  await wallpaper.set(path, (scale = "center"));
+async function setBackground(path) {
+  let file = path;
+  console.log(file);
+  await wallpaper.set(file, (scale = "center"));
   await wallpaper.get();
 }
 fetcher();
