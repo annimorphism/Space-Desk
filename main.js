fd5428113bf5a4cd62808514a48b78742b35fbe7
annimorphism <trivediannirudha@gmail.com>
2021-08-02 18:34:28 +0530

    Bugs Fixed and Refactored Code

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const fetch = require("node-fetch");
 const execSync = require("child_process").execSync;
 const wallpaper = require("wallpaper");
-const puppeteer = require("puppeteer");
 
 const key = process.env.api_key;
 let url = "https://api.nasa.gov/planetary/apod";
@@ -12,28 +11,21 @@ async function fetcher() {
   try {
     let response = await fetch(hit_url);
     let data = await response.json();
-    let img = data.hdurl ? data.hdurl : data.url;
-    let imgext = data.hdurl ? ".jpg" : ".png";
-    patt = `./Img/background${imgext}`;
-    console.log(patt);
-    let x = img.includes("gltf");
-    if (x) {
-      async function screen() {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(img);
-        await page.setViewport({
-          width: 1920,
-          height: 1080,
-        });
-        await page.screenshot({ path: patt });
-        await browser.close();
-      }
-      screen();
-    } else {
-      execSync(`curl -o ${patt} ${img}`);
+    let imgHd = data.hdurl ? data.hdurl : "";
+    let img = data.url ? data.url : "";
+    let imgext = ".jpg";
+    path = `./Img/background${imgext}`;
+    console.log(imgHd);
+    console.log(img);
+    if (imgHd.includes("jpg")) {
+      execSync(`curl -o ${path} ${imgHd}`);
+      setBackground(path);
+      console.log("HD url");
+    } else if (img.includes("jpg")) {
+      execSync(`curl -o ${path} ${img}`);
+      setBackground(path);
+      console.log("Normal url");
     }
-    setBackground(patt);
   } catch (e) {
     console.log("There was some error -> ", e);
   }
