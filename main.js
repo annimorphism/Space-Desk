75d71535351ccf56d6c9a717293474e26c89d08e
annimorphism <trivediannirudha@gmail.com>
2022-04-06 22:03:44 +0530

    Update: Saved Image name updated to real img name instead of default name

--- main.js
+++ main.js
@@ -8,32 +8,35 @@ let url = "https://api.nasa.gov/planetary/apod";
 let hit_url = url + `?api_key=${key}`;
 
 async function fetcher() {
-  try {
-    let response = await fetch(hit_url);
-    let data = await response.json();
-    let imgHd = data.hdurl ? data.hdurl : "";
-    let img = data.url ? data.url : "";
-    let imgext = ".jpg";
-    path = `./Img/background${imgext}`;
-    if (imgHd !== "") console.log(`HD Image URL is --> ${imgHd}`);
-    console.log(`Normal Image URL is --> ${img}`);
-    if (imgHd.includes("jpg")) {
-      execSync(`curl -o ${path} ${imgHd}`);
-      setBackground(path);
-      console.log("HD url");
-    } else if (img.includes("jpg")) {
-      execSync(`curl -o ${path} ${img}`);
-      setBackground(path);
-      console.log("Normal url");
-    }
-  } catch (e) {
-    console.log("There was some error -> ", e);
-  }
+	try {
+		let response = await fetch(hit_url);
+		let data = await response.json();
+		let imgHd = data.hdurl ? data.hdurl : "";
+		let img = data.url ? data.url : "";
+		let imgext = ".jpg";
+
+		let title = imgHd.split("/");
+		let path = `./Img/${title[title.length - 1]}`;
+
+		if (imgHd !== "") console.log(`HD Image URL is --> ${imgHd}`);
+		console.log(`Normal Image URL is --> ${img}`);
+		if (imgHd.includes("jpg")) {
+			execSync(`curl -o ${path} ${imgHd}`);
+			setBackground(path);
+			console.log("HD url");
+		} else if (img.includes("jpg")) {
+			execSync(`curl -o ${path} ${img}`);
+			setBackground(path);
+			console.log("Normal url");
+		}
+	} catch (e) {
+		console.log("There was some error -> ", e);
+	}
 }
 async function setBackground(path) {
-  let file = path;
-  console.log(file);
-  await wallpaper.set(file, (scale = "center"));
-  await wallpaper.get();
+	let file = path;
+	console.log(file);
+	await wallpaper.set(file, (scale = "center"));
+	await wallpaper.get();
 }
 fetcher();
